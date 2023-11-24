ae61ff1268274516aab3e656d765edfe9f4bdb84
YuLin-Coder <386869957@qq.com>
2023-11-24 18:16:18 +0800

    基于ssm的C2C二手交易平台管理系统

---
+++ src/main/java/com/wsk/controller/ForgetController.java
@@ -0,0 +1,103 @@
+package com.wsk.controller;
+
+import com.wsk.pojo.UserInformation;
+import com.wsk.pojo.UserPassword;
+import com.wsk.response.BaseResponse;
+import com.wsk.service.UserInformationService;
+import com.wsk.service.UserPasswordService;
+import com.wsk.tool.StringUtils;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wsk1103 on 2017/5/9.
+ */
+@RestController
+public class ForgetController {
+
+    @Resource
+    private UserPasswordService userPasswordService;
+    @Resource
+    private UserInformationService userInformationService;
+
+    @RequestMapping(value = "checkCode.do", method = {RequestMethod.POST, RequestMethod.GET})
+    public Map checkPhone(HttpServletRequest request, Model model,
+                          @RequestParam String code, @RequestParam String token) {
+        Map<String, Integer> map = new HashMap<>();
+        String name = request.getParameter("name");
+        String phone = request.getParameter("phone");
+        if (!StringUtils.getInstance().isNullOrEmpty(name)) {
+            request.getSession().setAttribute("name", name);
+            request.getSession().setAttribute("phone", phone);
+        }
+        String checkCodeToken = (String) request.getSession().getAttribute("token");
+        if (StringUtils.getInstance().isNullOrEmpty(checkCodeToken) || !checkCodeToken.equals(token)) {
+            map.put("result", 0);
+            return map;
+        }
+        //验证码错误
+        if (!checkCodePhone(code, request)) {
+            map.put("result", 0);
+            return map;
+        }
+        map.put("result", 1);
+        return map;
+    }
+
+    //更新密码
+    @RequestMapping("updatePassword.do")
+    public BaseResponse updatePassword(HttpServletRequest request, Model model,
+                                       @RequestParam String password, @RequestParam String token) {
+        //防止重复提交
+        String updatePasswordToken = (String) request.getSession().getAttribute("token");
+        if (StringUtils.getInstance().isNullOrEmpty(updatePasswordToken) || !updatePasswordToken.equals(token)) {
+            return BaseResponse.fail();
+        }
+        String realPhone = (String) request.getSession().getAttribute("phone");
+        UserPassword userPassword = new UserPassword();
+        String newPassword = StringUtils.getInstance().getMD5(password);
+        int uid;
+        try {
+            uid = userInformationService.selectIdByPhone(realPhone);
+            if (uid == 0) {
+                return BaseResponse.fail();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return BaseResponse.fail();
+        }
+        int id = userPasswordService.selectByUid(uid).getId();
+        userPassword.setId(id);
+        userPassword.setUid(uid);
+        userPassword.setModified(new Date());
+        userPassword.setPassword(newPassword);
+        int result;
+        try {
+            result = userPasswordService.updateByPrimaryKeySelective(userPassword);
+        } catch (Exception e) {
+            return BaseResponse.fail();
+        }
+        //更新失败
+        if (result != 1) {
+            return BaseResponse.fail();
+        }
+        UserInformation userInformation = userInformationService.selectByPrimaryKey(uid);
+        request.getSession().setAttribute("userInformation", userInformation);
+        return BaseResponse.success();
+    }
+
+    //check the phone`s code
+    private boolean checkCodePhone(String codePhone, HttpServletRequest request) {
+        String trueCodePhone = "12251103";
+        return codePhone.equals(trueCodePhone);
+    }
+}
---
+++ src/main/java/com/wsk/token/TokenProccessor.java
@@ -0,0 +1,54 @@
+package com.wsk.token;
+
+/**
+ * Created by wsk1103 on 2017/4/28.
+ */
+import org.apache.commons.codec.binary.Base64;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+public class TokenProccessor {
+
+    /*
+*单例设计模式（保证类的对象在内存中只有一个）
+*1、把类的构造函数私有
+*2、自己创建一个类的对象
+*3、对外提供一个公共的方法，返回类的对象
+*/
+    private TokenProccessor() {
+    }
+
+    private static final TokenProccessor instance = new TokenProccessor();
+
+    /**
+     * 返回类的对象
+     *
+     * @return
+     */
+    public static TokenProccessor getInstance() {
+        return instance;
+    }
+
+    /**
+     * 生成Token
+     * Token：Nv6RRuGEVvmGjB+jimI/gw==
+     *
+     * @return
+     */
+    public String makeToken() {  //checkException
+        //  7346734837483  834u938493493849384  43434384
+        String token = (System.currentTimeMillis() + new Random().nextInt(999999999)) + "";
+        //数据指纹   128位长   16个字节  md5
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte md5[] = md.digest(token.getBytes());
+            //base64编码--任意二进制编码明文字符   adfsdfsdfsf
+
+            return Base64.encodeBase64String(md5);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
